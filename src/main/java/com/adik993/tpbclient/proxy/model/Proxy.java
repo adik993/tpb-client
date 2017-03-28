7a1ba97f7211e624d123be6b1ca550155a7e2b66
Adrian S <adik993@gmail.com>
2017-03-28 23:58:05 +0200

    Added default constructor to Proxy

--- src/main/java/com/adik993/tpbclient/proxy/model/Proxy.java
+++ src/main/java/com/adik993/tpbclient/proxy/model/Proxy.java
@@ -1,13 +1,17 @@
 package com.adik993.tpbclient.proxy.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * Created by adrian on 28/03/17.
  */
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Proxy {
     private String domain;
     private float speed;
